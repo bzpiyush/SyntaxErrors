f4d894930cd23b822ff88ca157a01c841d84e210
ayushdev351 <mrayushchauhan351@gmail.com>
2022-02-11 21:14:48 +0530

    Initialised React App

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Home.css';
+
+function Home() {
+    return(
+        <div className="Home">
+            Hello From Home Change here.....
+        </div>
+    );
+}
+
+export default Home;
