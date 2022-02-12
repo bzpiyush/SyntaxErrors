3ea48b3d6d274eedbbda63bbbb3131807b442f54
Piyush <piyushpm964@gmail.com>
2022-02-12 22:53:48 +0530

    Aded files

--- client/src/components/PatientPage.js
+++ client/src/components/PatientPage.js
@@ -1,11 +1,42 @@
 import React from "react";
+
 import PatientCard from "../cards/PatientCard";
 
 import './PatientPage.css'
 
-function PatientPage() {
+function PatientPage() 
+{
+  
+  const submithandler=(e)=>{
+    e.preventDefault();
+    fetch('http://localhost:5000/enterpatient', {
+  method: 'POST', // or 'PUT'
+  headers: {
+    'Content-Type': 'application/json',
+  },
+
+  body: JSON.stringify({
+    "hid" : "abcd"
+  })
+})
+
+    .then(response => response.json())
+    .then(data => {
+    
+    console.log("HI HELLO");
+})
+.catch((error) => {
+  //console.error('Error:', error);
+});
+    
+  }
+  
+  
+
+
   return(
       <div className="patientCards">
+        <button onClick={submithandler}>Hello</button>
         <PatientCard 
           patientName= "John Singh"
           patientMobile="9718983031"
